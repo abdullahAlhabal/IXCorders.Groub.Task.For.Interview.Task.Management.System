99bb3c4d6ddbf8c542ff99dd8464efaeef6c11e4
Abdullah Alhabal <dbnkalhbalb@hotmail.com>
2024-04-25 19:27:45 +0300

    Insert One Million Record into the Database's Task Table using Seeding
    It takes about 45 ~ 50 minutes in my Machine D;

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,8 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+        User::factory(10)->create();
+
+        Task::truncate();
+
+        $total = 1000000;
+        $batchSize = 1000;
+
+        for ($i = 0; $i < $total; $i += $batchSize) {
+            $tasks = Task::factory($batchSize)->make();
+            Task::insert($tasks->toArray());
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Task;
-use Database\Factories\TaskFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,7 +17,15 @@ class TaskSeeder extends Seeder
     {
         // Task::truncate();
         // DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        // Re-enable foreign key constraints
+
+        // $total = 1000000;
+        // $batchSize = 1000;
+
+        // for ($i = 0; $i < $total; $i += $batchSize) {
+        //     $tasks = Task::factory($batchSize)->make();
+        //     Task::insert($tasks->toArray());
+        // }
+
         // DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
